3c0d5976af301d1109cfdad55d3970dd235fd3eb
evandschaffer <evandschaffer@gmail.com>
2025-07-07 10:16:10 -0230

    Tab Class

--- src/main/java/Tab.java
+++ src/main/java/Tab.java
@@ -1,4 +1,8 @@
-public class Fretboard {
+public class Tab {
+  private Note note;
+  private int str;
+  private int fret;
+  private int[] range; //[lower fret, upper fret, lower string, upper string]
   private static String[][] fb = {
     {"e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#", "e"},
     {"a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a"},
@@ -8,7 +12,14 @@ public class Fretboard {
     {"e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#", "e"}
   };
 
-  public int[] getTabs(Note[] notes) { //[string, fret]
+  public Tab(Note note, int[] range) {
+    this.note = note;
+    this.range = range;
+    this.str = tabOut()[0];
+    this.fret = tabOut()[1];
+  }
+
+  public int[] tabOut() {
     return null;
   }
 }
