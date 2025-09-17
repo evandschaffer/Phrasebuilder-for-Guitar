40a4691db960cb3a1cc1c2fad1b4c39848a53aee
Evan Schaffer <EvanDSchaffer@gmail.com>
2025-09-17 14:46:39 -0230

    CSV addition and GUI layout change

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,5 +1,5 @@
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
+import javax.swing.*;
+import java.awt.*;
 
 public class UserInterface {
     public UserInterface() {
@@ -8,8 +8,39 @@ public class UserInterface {
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setSize(1080, 720);
       frame.setResizable(false);
-      frame.setVisible(true);
       ImageIcon image = new ImageIcon("PB.png");
       frame.setIconImage(image.getImage());
+      frame.setLayout(new GridLayout(8,24));
+
+      JLabel label = new JLabel();
+      label.setText("Welcome to Phrasebuilder for Guitar!");
+      label.setHorizontalAlignment(JLabel.CENTER);
+      label.setVerticalAlignment(JLabel.TOP);
+      frame.add(label);
+
+      JButton start = new JButton("START");
+      start.setBounds(0, 0, 0, 0);
+      frame.add(start);
+
+
+      frame.setVisible(true);
     }
+  private void switchPage(int page) {
+    switch(page) {
+      case 1:
+        //scale selection
+        break;
+      case 2:
+        //phrase settings
+        break;
+      case 3:
+        //tab settings
+        break;
+      case 4:
+        //final output
+        break;
+      default:
+        break;
+    }
+  }
 }
